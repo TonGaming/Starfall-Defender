34e126221dd6db5911f03bdd56ac68f6c833957a
LadyKillerr <dungvuduc0509@gmail.com>
2023-11-03 23:30:32 +0700

    fix and feat: fixed movement bugs, added map boundaries, prefabbed enemy path and created some scriptable objects for path finding

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,46 +7,80 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 2f;
 
+    [SerializeField] float paddingTop = 1f;
+    [SerializeField] float paddingBottom = 1f;
+    [SerializeField] float paddingLeft = 1f;
+    [SerializeField] float paddingRight = 1f;
+
 
     Vector2 moveInput;
-    Vector3 delta;
+
+    // Vector toạ độ để giới hạn sự di chuyển của người chơi
+    Vector2 minBounds;
+    Vector2 maxBounds;
 
     Rigidbody2D playerRigidbody;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
-        
+
+        // kiểm tra giới hạn ngay từ frame đầu tiên 
+        InitializationBounds();
+
+
+
+        // lấy ra 
     }
 
     // Update is called once per frame
     void Update()
     {
         Fly();
-
     }
+
+    void InitializationBounds()
+    {
+        Camera mainCamera = Camera.main;
+
+        // mặc dù phải truyền vào Vector3 nhưng nếu ta truyền vào Vector2 thì default toạ độ trục Z sẽ =  0 luôn
+        // cta bỏ qua trục Z vì đây là game 2D, nên chỉ cần Vector2 là đủ
+
+        // điểm dưới trái của ViewPort (0, 0)
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0,0)) ;
+        // điểm trên phải của ViewPort (1,1)
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>() ;
         
+
     }
 
     void Fly()
     {;
-        // Lấy ra giá trị -1 1 của trục X trục Y của input và gán vào delta
-        //delta = moveInput * moveSpeed;
-        
-        //float shipPositionX = transform.position.x;
-        //shipPositionX += moveInput.x * moveSpeed;
 
-        //float shipPositionY = transform.position.y;
-        //shipPositionY += moveInput.y * moveSpeed;
-
-        // transform.position = new Vector2 (shipPositionX, shipPositionY);
-
+        // di chuyển player = velocity của rigidbody
         playerRigidbody.velocity = new Vector2(moveInput.x * moveSpeed , moveInput.y * moveSpeed );
+
+        // tạo một biến và gán vị trí của player vào đó để kiểm tra xem có vượt ra ngoài ViewPort không
+        Vector2 newPos = new Vector2();
+
+        // kiểm tra giá trị toạ độ trục X - Hàm Clamp là sẽ loại bỏ tất cả giá trị truyền vào vượt quá 
+        newPos.x = Mathf.Clamp(transform.position.x , minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+
+        // kiểm tra giá trị toạ độ trục Y
+        newPos.y = Mathf.Clamp(transform.position.y , minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+        transform.position = newPos;
+
+
+
 
     }
 }
---
+++ Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu ( menuName = "Wave Config", fileName = "New Wave Config")]
+
+public class WaveConfigSO : ScriptableObject
+{
+    // để cài prefabs path vào 
+    [SerializeField] Transform pathPrefab;
+    [SerializeField] float enemyMoveSpeed;
+    
+    public float GetEnemyMoveSpeed()
+    {
+        return enemyMoveSpeed;
+    }
+    
+}
