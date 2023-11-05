c5f5175593609a2d6d1d2144cc6fd98fb6573504
LadyKillerr <dungvuduc0509@gmail.com>
2023-11-05 18:32:22 +0700

    feat: added enemy path and movement

---
+++ Assets/Scripts/PathFinder.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour
+{
+    [SerializeField] WaveConfigSO waveConfig;
+
+
+
+    List<Transform> waypoints ;
+
+    int waypointsIndex = 0;
+
+
+    void Start()
+    {
+
+        // sau đó gán list waypoints đã có sẵn vào list waypoints mới tạo bên này
+        waypoints = waveConfig.GetWaypoints();
+
+        //  vị trí của bdau enemy = vị trí của waypoints đầu tiên 
+        transform.position = waypoints[waypointsIndex].position;
+
+
+    }
+
+    void Update()
+    {
+        PathRunner();
+    }
+
+    // để làm enemy di chuyển cần có: vị trí hiện tại, điểm đến kế tiếp, hướng di chuyển, tốc độ di chuyển
+    void PathRunner()
+    {
+        // nếu vẫn còn path chưa đi hết
+        if (waypointsIndex < waypoints.Count)
+        {
+
+            // gán điểm đến tiếp theo vào biến vector2
+            Vector2 targetPosition = waypoints[waypointsIndex].position;
+
+            // khoảng cách di chuyển giữa các frame
+            float movedEachFrame = waveConfig.GetEnemyMoveSpeed() * Time.deltaTime;
+
+            // sử dụng MoveTowards để làm vật di chuyển về 1 hướng nhất định vs 1 speed tuỳ chỉnh
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movedEachFrame);
+
+            // nếu đã đạt tới vị trí của điểm đến tiếp theo, ta tăng waypointsIndex để hướng tới vị trí kế
+            if(transform.position == waypoints[waypointsIndex].position)
+            {
+                waypointsIndex++;
+            }
+
+        }
+        // nếu đã đi hết path
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/WaveConfigSO.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu ( menuName = "Wave Config", fileName = "New Wave Config")]
+
+public class WaveConfigSO : ScriptableObject
+{
+    // để cài prefabs path vào 
+    [SerializeField] Transform pathPrefab;
+    [SerializeField] float enemyMoveSpeed;
+    
+    public float GetEnemyMoveSpeed()
+    {
+        return enemyMoveSpeed;
+    }
+
+    public Transform GetStartingWaypoint()
+    {
+        return pathPrefab.GetChild(0);
+    }
+    
+    public List<Transform> GetWaypoints()
+    {
+        // tạo một list rỗng để lưu waypoints vào 
+        List<Transform> waypointsList = new List<Transform>();
+
+        // mỗi khi lặp qua một phần tử transform trong pathPrefabs thì Add phần tử đó vào list vừa tạo
+        foreach (Transform child in pathPrefab)
+        {
+            waypointsList.Add(child);
+        }
+
+        return waypointsList;
+    }
+}
+
+
+
+
