f9cf3f3b2dfb7338a7937df0171e813cf12c09b1
LadyKillerr <dungvuduc0509@gmail.com>
2023-11-08 11:10:38 +0700

    feat: added player health and mortality

---
+++ Assets/Scripts/DamageDealer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    [SerializeField] int damage = 10;
+
+    public int GetDamage()
+    {
+        return damage;
+    }
+
+    public void Explode()
+    {
+        Destroy(gameObject);
+    }
+
+
+
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] int health = 50;
+
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // check xem có chạm vào Damage Dealer k để trừ máu
+        // Nếu chạm vào vật thể có DamageDealer thì sẽ lưu tạm vào biến damageDealer
+        // còn nếu không thì damageDealer sẽ là null
+        DamageDealer damageDealer = other.GetComponent<DamageDealer>();
+
+        if (damageDealer != null)
+        {
+            // ăn dame và trừ máu
+            TakeDamage(damageDealer.GetDamage());
+
+            // kẻ địch sẽ bị huỷ đi khi chạm vào ng chơi
+            damageDealer.Explode();
+
+
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+}
