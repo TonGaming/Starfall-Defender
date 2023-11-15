85bc789f9cae816873dc2e688bcb4a0f386c068a
LadyKillerr <dungvuduc0509@gmail.com>
2023-11-15 18:12:07 +0700

    feat: added Parallax background in game, deleted the old starfield fixed background

---
+++ Assets/Scripts/SpriteScroller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteScroller : MonoBehaviour
+{
+    [SerializeField] Vector2 moveSpeed;
+
+    Vector2 offset;
+    Material material;
+
+    void Awake()
+    {
+        // get ra material của background hiện tại
+        material = GetComponent<SpriteRenderer>().material;
+    }
+
+    void Update()
+    {
+        MoveBackground();
+    }
+
+    void MoveBackground()
+    {
+        // offset của material nằm trong các layer background sẽ được tăng dần lên theo từng frame
+        offset = moveSpeed + offset;
+        
+        // chỉnh phần offset của material thông qua mainTextureOffset
+        material.mainTextureOffset = offset;
+    }
+}
