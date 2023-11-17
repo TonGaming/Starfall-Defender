4a1fa984aad9fcbe25f00f03bc1f0f36e0fc0323
LadyKillerr <dungvuduc0509@gmail.com>
2023-11-17 17:44:33 +0700

    feat: added functional UI

---
+++ Assets/Scripts/UIDisplay.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDisplay : MonoBehaviour
+{
+    Health playerHealth;
+    ScoreKeeper playerScore;
+
+    [SerializeField] Slider playerHealthBarUI;
+    [SerializeField] TextMeshProUGUI playerScoreText;
+
+    private void Awake()
+    {
+        playerHealth = FindObjectOfType<Player>().GetComponent<Health>();
+        playerScore = FindObjectOfType<ScoreKeeper>();
+    }
+    
+    void Start()
+    {
+        playerHealthBarUI.maxValue = playerHealth.GetHealth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerHealthBarUI.value = playerHealth.GetHealth();
+
+
+
+        playerScoreText.text = playerScore.GetCurrentScore().ToString("00000000");
+    }
+
+
+}
