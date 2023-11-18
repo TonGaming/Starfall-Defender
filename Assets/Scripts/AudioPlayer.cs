f46063a53104ccb8ee6c7f65dd7146cee4ee311d
LadyKillerr <dungvuduc0509@gmail.com>
2023-11-18 17:28:01 +0700

    feat: Singleton Pattern for audio Player

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -16,6 +16,33 @@ public class AudioPlayer : MonoBehaviour
     [SerializeField] AudioClip GotShotClip;
     [SerializeField][Range(0, 1)] float gotShotVolume = 1f;
 
+    // static variables persists on all of the same type of gameObject in the scene
+    // static AudioPlayer instance;
+
+
+    //Singleton Handling
+    private void Awake()
+    {
+        ManageSingleton();
+    }
+
+    void ManageSingleton()
+    {
+        int instanceCount = FindObjectsOfType(GetType()).Length; 
+        if (instanceCount > 1)
+        {
+            // destroy game object only works when a frame ends, to prevent anything trying to grab this game object 
+            // that we disabled it, which is execute right on the spot
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+
     // Hàm chung cho tiện
     private void PlayClip (AudioClip clip, float volume)
     {
